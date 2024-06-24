01fb5b07823a4bc9d35a894ee4640061c88701cb
GabrielFerrin <gabrielferrin@byui.edu>
2024-06-24 17:22:50 -0500

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import './App.css'
+import useCatFact from './hooks/useCatFact'
+import { useCatImage } from './hooks/useCatImage'
 
 function App() {
+  const { fact, refreshFact } = useCatFact()
+  const { imageUrl } = useCatImage({ fact })
   return (
     <>
+      <h1>App de gatos</h1>
+      <button onClick={refreshFact}>Get new fact</button>
+      {fact && <p>{fact}</p>}
+      {
+        imageUrl && <img src={imageUrl} width={300}
+          alt={`Image extracted using the first three words for ${fact}`} />
+      }
     </>
   )
 }
